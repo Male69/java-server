b158737487fc3e47f73b582d7e5f5727ca6dd9ab
Maciej Jurczyk <mjurczyk@libertyglobal.com>
2015-03-03 21:02:10 +0100

    Code clean-up

--- src/pl/porscheLambo/clientServer/Item.java
+++ src/pl/porscheLambo/clientServer/Item.java
@@ -20,24 +20,19 @@ public class Item {
 	}
 
 	public static void main(String[] args) {
-		//Item obj1 = new Item(5, "jarek");
-		//obj1.print();
-		//Item obj2 = new Item(10, "kuba");
-		//obj2.print();
 		List<String> tab1 = new ArrayList<String>();
 		List<String> tab2 = new ArrayList<String>();
+
 		tab2.add("jeden");
 		tab2.add("jeden");
 		tab2.add("jeden");
+
 		tab1 = tab2;
-		String result = null ;
-		//for (String elem : tab1) {
-			result = String.join(":", tab1);
-		//}
+
+		String result = null;
+		result = String.join(":", tab1);
+
 		System.out.println("Print:" + tab1.get(0) + tab1.get(1) + tab1.get(2) );
 		System.out.println(result);
-		//tab2.clear();
-		//tab2.add(result.split(":"));
 	}
-
 }
